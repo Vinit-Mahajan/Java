b0b2bd948b8892cdec23197dffca1179f55119f6
Vinit-Mahajan <mvinit923@gmail.com>
2023-05-17 16:00:51 +0530

    Assigment of NestedFor1 is done

---
+++ Assignment/NestedFor1/Example6.java
@@ -0,0 +1,22 @@
+/*
+
+   9 8 7
+   9 8 7
+   9 8 7
+*/
+
+class Example6 {
+
+	public static void main(String[] args) {
+	
+		for(int i = 1 ; i <= 3 ; i++) {
+
+			int num = 9;
+			for(int j = 1 ; j <= 3 ; j++) {
+			
+				System.out.print((num-j+1) + " ");
+			}
+			System.out.println();
+		}
+	}
+}
